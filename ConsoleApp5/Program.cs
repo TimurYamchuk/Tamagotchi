799b0b2216428f7a4a165ca3c3b2f187d7042f78
TimurYamchuk <134388477+TimurYamchuk@users.noreply.github.com>
2024-11-11 00:18:45 +0300

    Update Program.cs

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,10 +1,52 @@
-﻿using System;
+using System;
 
-class Program
+public class Program
 {
-    static void Main()
+    public static void Main()
     {
         Tamagotchi tamagotchi = new Tamagotchi();
+
+        // Создание наблюдателей
+        var requestObserver = new RequestObserver();
+        var healthObserver = new HealthObserver();
+        var lifeCycleObserver = new LifeCycleObserver();
+
+        // Подписка наблюдателей на события
+        tamagotchi.OnRequest += requestObserver.OnRequestReceived;
+        tamagotchi.OnSick += healthObserver.OnSick;
+        tamagotchi.OnHealed += healthObserver.OnHealed;
+        tamagotchi.OnLifeEnd += lifeCycleObserver.OnLifeEnd;
+
         tamagotchi.StartLife();
+    }
+}
+
+// Наблюдатели
+public class RequestObserver
+{
+    public void OnRequestReceived(string request)
+    {
+        Console.WriteLine($"Наблюдатель: Тамагочи запросил {request}.");
+    }
+}
+
+public class HealthObserver
+{
+    public void OnSick()
+    {
+        Console.WriteLine("Наблюдатель: Тамагочи заболел!");
+    }
+
+    public void OnHealed()
+    {
+        Console.WriteLine("Наблюдатель: Тамагочи выздоровел!");
+    }
+}
+
+public class LifeCycleObserver
+{
+    public void OnLifeEnd()
+    {
+        Console.WriteLine("Наблюдатель: Жизненный цикл Тамагочи завершён.");
     }
 }
